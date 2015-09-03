e75824ac1380a8277d6257efbdcf490a1c4dabf5
Analcr1996 <anacollantes@yahoo.com>
2015-09-03 17:45:58 -0400

    changes on the width&height and the x,y
    I changed the size of the circle to an oval shape and position it more on the center. Also, I moved the text to be below the oval shape and in the center.

--- circles0.java
+++ circles0.java
@@ -1,15 +1,14 @@
-
 //// Wild circles #0.
 
 // Setup:  screen size. //
 void setup() {
-  size(500, 100);                // Try changing the width & height.
+  size(400, 400);                // Try changing the width & height.
 }
 
 // Next frame:  circle at (x,y). //
 void draw() {
-  ellipse( 50,50, 30,30 );       // Try changing the (x,y) and size.
+  ellipse( 200,200, 300,200 );       // Try changing the (x,y) and size.
 
-text ( "ha ha", 50, height-50);
+text ( "ha ha", 200, height-85);
 
 }
