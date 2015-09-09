07c30cc80de9025f8f75a37dadd84aaab8552b8b
Analcr1996 <anacollantes@yahoo.com>
2015-09-09 16:05:34 -0400

    I created a creature
    I created a creature with a head and oval body, with eyes, and a rectangular mouth. I named my creature Jimin.

---
+++ circles3.java
@@ -0,0 +1,27 @@
+void setup() {
+  size(700, 800);
+
+background(570);
+
+fill(500, 200, 100);
+ellipse( width/3, height/3, 150, 150);
+
+ellipse( width/2.99, height/1.63, 200, 300);
+
+fill(255);
+ellipse(width/2.7, height/3.2, 20, 20);
+ellipse(width/3.4, height/3.2, 20, 20);
+
+
+fill(65);
+rect( width/3.13, height/2.8, 20, 20);
+
+fill(102);
+ellipse(width/2.70, height/3.2, 10, 10);
+ellipse(width/3.40, height/3.2, 10, 10);
+
+text( "Jimin", width/3.19, height/1.65);
+
+fill(255);
+text( "By: Ana Collantes" , width/18, height/1.26);
+}
